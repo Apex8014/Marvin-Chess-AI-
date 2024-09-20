828fc1ac88e40505bcaf51d8bf8105658bf78370
Apex8014 <121900510+Apex8014@users.noreply.github.com>
2024-09-20 12:44:33 -0700

    Fixing file path

--- Marvin(Chess AI)/King.py
+++ Marvin(Chess AI)/King.py
@@ -4,3 +4,12 @@ class King(Piece):
     def __init__(self,color):
         super().__init__(color)
         self.type = "K"
+
+    def validMoves(self,board):
+        pos = self.getPos(board)
+        self.movesList = []
+        #Basic movement
+        for y in range(-1,1,1):
+            for x in range(-1,1,1):
+                if (x == 0 and y == 0 and board.ChessBoard[pos[1]+y][pos[0]+x].type != self.type):
+                    self.movesList[len(self.movesList)] = (pos[1]+y,pos[0]+x)
