5a1b1c4311d491f68b0d5aa3f75e91034c8690bd
Apex8014 <121900510+Apex8014@users.noreply.github.com>
2024-09-19 18:15:47 -0700

    Add files via upload
    Added valid moves function

--- Marvin(Chess AI)/Knight.py
+++ Marvin(Chess AI)/Knight.py
@@ -4,3 +4,12 @@ class Knight(Piece):
     def __init__(self,color):
         super().__init__(color)
         self.type = "N"
+
+    def validMoves(self,board):
+        self.movesList = []
+        pos = self.getPos(board)
+        for x in range(-2,2,1):
+            for y in range(-2,2,1):
+                if (abs(x)+abs(y) == 3 and board.ChessBoard[y][x].color != self.color):
+                    self.movesList[len(self.movesList)] = (x,y)
+        return self.movesList
