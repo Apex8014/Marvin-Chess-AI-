7f8f2d8db20a75022b951176a08b481944ba9873
Apex8014 <121900510+Apex8014@users.noreply.github.com>
2024-09-20 17:58:21 -0700

    Added getAttackedSquares, updateAttackedSquares, changed knight to fit previous implementation(validMoves)

--- Marvin(Chess AI)/Piece.py
+++ Marvin(Chess AI)/Piece.py
@@ -3,6 +3,13 @@ class Piece:
         self.color = color
         self.type = "Piece"
         self.hasMoved = False
+        self.attackedSquares = []
+        self.blackPieces = []
+        self.whitePieces = []
+        if color == "White":
+            self.whitePieces.append(self)
+        if color == "Black":
+            self.blackPieces.append(self)
 
     def getPos(self, board):
         for x in range(8):
@@ -10,3 +17,16 @@ class Piece:
                 if board[y][x] is self:
                     return (x, y)
         return None
+    
+    def updateAttackedSquares(self,board):
+        self.attackedSquares = self.validMoves(board)
+
+    def getAttackedSquares(self):
+        self.squaresUnderAttack = []
+        if self.color == "White":
+            for i in self.blackPieces:
+                self.squaresUnderAttack = sum([i.attackedSquares,self.squaresUnderAttack],[])
+        else:
+            for i in self.whitePieces:
+                self.squaresUnderAttack = sum([i.attackedSquares,self.squaresUnderAttack],[])
+        return self.squaresUnderAttack
