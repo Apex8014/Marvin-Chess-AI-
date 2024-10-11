1fa36e95cf582ff80949c19e35403d94a8b11636
Apex8014 <121900510+Apex8014@users.noreply.github.com>
2024-10-11 13:56:33 -0700

    Fixed Knight validMove Code

--- Marvin(Chess AI)/Knight.py
+++ Marvin(Chess AI)/Knight.py
@@ -8,8 +8,9 @@ class Knight(Piece):
     def validMoves(self,board):
         self.movesList = []
         pos = self.getPos(board)
-        for x in range(-2,2,1):
-            for y in range(-2,2,1):
+        for x in range(-2,3,1):
+            for y in range(-2,3,1):
                 if (abs(x)+abs(y) == 3 and board[pos[1]+y][pos[0]+x].color != self.color):
-                    self.movesList.append((pos[1]+y,pos[0]+x))
+                    self.movesList.append((pos[0]+x,pos[1]+y))
+        
         return self.movesList
