b74c0c8aa1f636fe4ad57b2ffb10656e529501a8
Apex8014 <121900510+Apex8014@users.noreply.github.com>
2024-10-16 10:00:40 -0700

    Fixed bug where it would check out of bounds

--- Marvin(Chess AI)/Knight.py
+++ Marvin(Chess AI)/Knight.py
@@ -10,7 +10,8 @@ class Knight(Piece):
         pos = self.getPos(board)
         for x in range(-2,3,1):
             for y in range(-2,3,1):
-                if (abs(x)+abs(y) == 3 and board[pos[1]+y][pos[0]+x].color != self.color):
-                    self.movesList.append((pos[0]+x,pos[1]+y))
+                if 8 > pos[1]+y > -1 and 8 > pos[0]+x > -1:
+                    if (abs(x)+abs(y) == 3 and board[pos[1]+y][pos[0]+x].color != self.color):
+                        self.movesList.append((pos[0]+x,pos[1]+y))
         
         return self.movesList
