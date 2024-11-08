ddc8541427210011f7015f5787bf1d659884ec66
Apex8014 <121900510+Apex8014@users.noreply.github.com>
2024-11-08 15:47:29 -0800

    Optimized Queen

--- Marvin(Chess AI)/Queen.py
+++ Marvin(Chess AI)/Queen.py
@@ -8,77 +8,68 @@ class Queen(Piece):
     def validMoves(self, board):
         pos = self.getPos(board)
         self.movesList= []
-        for x in range(8):
-            #checks all possible rightward moves
-            if (x == 0):
-                continue
-            if (x+pos[0] < 8):
-                if (board[pos[1]][pos[0]+x].color == self.color):
-                    break
-                else: 
-                    self.movesList.append((pos[0]+x,pos[1]))
-                    #allows the loop to keep going if there is an empty space, but ends it if taking a piece is the las possible option
-                    if (board[pos[1]][pos[0]+x].color != " "):
-                        break
-            else:
-                break
-        
-        
-        for x in range(8):
-            #checks all possible leftward moves
-            if (x == 0):
-                continue
-            if (pos[0]-x > -1):
-                if (board[pos[1]][pos[0]-x].color == self.color):
-                    #print(pos[0]-x)
-                    break
-                else: 
-                    self.movesList.append((pos[0]-x,pos[1]))
-                    #allows the loop to keep going if there is an empty space, but ends it if taking a piece is the las possible option
-                    if (board[pos[1]][pos[0]-x].color != " "):
-                        break
-            else:
-                break
-        
-        
-        for y in range(8):
-            #checks all possible upward moves
-            if (y == 0):
-                continue
-            if (y+pos[1] < 8):
-                if (board[pos[1]+y][pos[0]].color == self.color):
-                    break
-                else: 
-                    self.movesList.append((pos[0],pos[1]+y))
-                    #allows the loop to keep going if there is an empty space, but ends it if taking a piece is the las possible option
-                    if (board[pos[1]+y][pos[0]].color != " "):
-                        break
-            else:
-                break
-        
-
-        for y in range(8):
-            #checks all possible upward moves
-            if (y == 0):
-                continue
-            if (pos[1] - y > -1):
-                if (board[pos[1]-y][pos[0]].color == self.color):
-                    break
-                else: 
-                    self.movesList.append((pos[0],pos[1]-y))
-                    #allows the loop to keep going if there is an empty space, but ends it if taking a piece is the las possible option
-                    if (board[pos[1]-y][pos[0]].color != " "):
-                        break
-            else:
-                break
-
         self.upRight = True
         self.downRight = True
         self.upLeft = True
         self.downLeft = True
+        right = True
+        left = True
+        up = True
+        down = True
         for d in range(8):
             if d == 0:
                 continue
+            #checks all possible rightward moves
+            if right:
+                if (d+pos[0] < 8):
+                    if (board[pos[1]][pos[0]+d].color == self.color):
+                        right = False
+                    else: 
+                        self.movesList.append((pos[0]+d,pos[1]))
+                        #allows the loop to keep going if there is an empty space, but ends it if taking a piece is the las possible option
+                        if (board[pos[1]][pos[0]+d].color != " "):
+                            right = False
+                else:
+                    right = False
+
+            #checks all possible leftward moves
+            if left:
+                if (pos[0]-d > -1):
+                    if (board[pos[1]][pos[0]-d].color == self.color):
+                        left = False
+                    else: 
+                        self.movesList.append((pos[0]-d,pos[1]))
+                        #allows the loop to keep going if there is an empty space, but ends it if taking a piece is the las possible option
+                        if (board[pos[1]][pos[0]-d].color != " "):
+                            left = False
+                else:
+                    left = False
+            
+            #checks all possible upward moves
+            if up:
+                if (d+pos[1] < 8):
+                    if (board[pos[1]+d][pos[0]].color == self.color):
+                        up = False
+                    else: 
+                        self.movesList.append((pos[0],pos[1]+d))
+                        #allows the loop to keep going if there is an empty space, but ends it if taking a piece is the las possible option
+                        if (board[pos[1]+d][pos[0]].color != " "):
+                            up = False
+                else:
+                    up = False
+
+            #checks all possible downward moves
+            if down:
+                if (pos[1] - d > -1):
+                    if (board[pos[1]-d][pos[0]].color == self.color):
+                        down = False
+                    else: 
+                        self.movesList.append((pos[0],pos[1]-d))
+                        #allows the loop to keep going if there is an empty space, but ends it if taking a piece is the las possible option
+                        if (board[pos[1]-d][pos[0]].color != " "):
+                            down = False
+                else:
+                    down = False
             #up-right
             if 8 > pos[0] + d > -1 and 8 > pos[1] + d > -1 and self.upRight:
                 if board[pos[1]+d][pos[0]+d].color != self.color:
