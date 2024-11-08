3926e56ab70f44bf9af3ef86eeae0617d37094f0
Apex8014 <121900510+Apex8014@users.noreply.github.com>
2024-11-08 14:45:47 -0800

    Game Optimization (Broken Checkmate Detection)
    comment out the checkmate detection, its broken rn.

--- Marvin(Chess AI)/Game.py
+++ Marvin(Chess AI)/Game.py
@@ -1,5 +1,6 @@
 from Board import Board
 from Player import Player
+import Global
 
 
 winner = ""
@@ -13,30 +14,25 @@ while(winner == ""):
     gameBoard.printBoard()
     print("White")
     gameBoard.updatePosition(WhitePlayer.playerMove(gameBoard.ChessBoard))
-    #if it finds the black king, it changes the winner value to "!", if winner is not "!", then the king is gone and therefore white wins
-    for x in range(8):
-        for y in range(8):
-            if gameBoard.ChessBoard[y][x].type == "K" and gameBoard.ChessBoard[y][x].color == "Black":
-                winner = "!"
-    if winner != "!":
-        winner = "White"
-        print("White Wins!")
-        break
-    else:
-        winner = ""
+    #if the king is in check and doesn't have any valid moves, checkmate (Does not currently account for moving a piece to prevent check/checkmate)
+    for i in Global.whitePieces:
+        print(i.type)
+        print(i.attackedSquares)
+        print(Global.kings[1].getPos(gameBoard.ChessBoard))
+        if Global.kings[1].getPos(gameBoard.ChessBoard) in i.attackedSquares and len(i.attackedSquares) > 0:
+            if len(Global.kings[1].validMoves(gameBoard.ChessBoard)) == 0:
+                winner = "White"
+                print("White Wins!")
+                break
     print("Black to move:")
     print("Black")
     gameBoard.printBoard()
     print("White")
     gameBoard.updatePosition(BlackPlayer.playerMove(gameBoard.ChessBoard))
     #if it finds the white king, it changes the winner value to "!", if winner is not "!", then the king is gone and therefore black wins
-    for x in range(8):
-        for y in range(8):
-            if gameBoard.ChessBoard[y][x].type == "K" and gameBoard.ChessBoard[y][x].color == "White":
-                winner = "!"
-    if winner != "!":
-        winner = "Black"
-        print("Black Wins!")
-        break
-    else:
-        winner = ""
+    for i in Global.blackPieces:
+        if Global.kings[0].getPos(gameBoard.ChessBoard) in i.attackedSquares:
+            if len(Global.kings[0].validMoves(gameBoard.ChessBoard)) == 0:
+                winner = "Black"
+                print("Black Wins!")
+                break
